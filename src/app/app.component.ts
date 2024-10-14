72d8225cfbddd8d057b67d54de643de7bc4337b5
MaruPratik80 <pratikmaru80@gmail.com>
2024-10-14 19:41:11 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-6';
+
+  form!: FormGroup;
+  submited = false;
+  countries = ['India', 'US', 'England'];
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+      lastName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+      email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(50)]),
+      gender: new FormControl(null, Validators.required),
+      married: new FormControl(false, Validators.required),
+      country: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    this.submited = true;
+    if (this.form.valid) console.log(this.form.value);
+  }
+
+  onReset() {
+    this.submited = false;
+    this.form.reset({
+      married: false,
+    });
+  }
 }
